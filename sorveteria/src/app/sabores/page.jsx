3f7057624515fc113553ef2dc0f9b8cf34ceb253
Marlon Santos <marlonadrianoteixeira@gmail.com>
2024-12-27 16:03:33 -0300

    pagina de sabores concluida

--- sorveteria/src/app/sabores/page.jsx
+++ sorveteria/src/app/sabores/page.jsx
@@ -4,7 +4,7 @@ import styles from "@/app/sabores/sabores.module.css"; // Se você tiver um esti
 export default function Sabores() {
   return (
     <section>
-      <div className="w3-display-container w3-text-white">
+      <div>
         {/* A imagem fica como fundo da "container", permitindo que o texto fique sobre ela */}
         <Image
           src="/banner-sabores.jpg"
@@ -15,8 +15,79 @@ export default function Sabores() {
         />
         <div className={styles["text-container"]}>
           {/* O w3-center vai garantir que o texto esteja centralizado */}
-          <h1>SABORES DE SORVETE</h1>
+          <h1>NOSSOS SABORES</h1>
         </div>
+      </div>
+
+      <h2 className={styles.h2}>SABORES DE SORVETE</h2>
+
+      <div className={styles.cards}>
+      <div className={styles.card_individual}>
+        <Image
+          src="/sabor-oreo.jpg"
+          alt="Sabor Oreo"
+          width={100}
+          height={100}
+          className={styles.image_card}
+        />
+        <h4>Sorvete de Oreo</h4>
+        <p>Delicioso sorvete sabor Oreo. Uma explosão de sabor.</p>
+      </div>
+      <div className={styles.card_individual}>
+      <Image
+          src="/sorbet-morango.png"
+          alt="Sabor Morango"
+          width={100}
+          height={100}
+          className={styles.image_card}
+        />
+        <h4>Sorvete Morando</h4>
+        <p>Delicioso sorvete sabor Morango. Uma explosão de sabor.</p>
+      </div>
+      <div className={styles.card_individual}>
+      <Image
+          src="/sabor-pistache.png"
+          alt="Sabor Oreo"
+          width={100}
+          height={100}
+          className={styles.image_card}
+        />
+        <h4>Sorvete Pistache</h4>
+        <p>Cremoso sorvete sabor Pistache. Uma explosão de sabor.</p>
+      </div>
+      <div className={styles.card_individual}>
+      <Image
+          src="/sabor-cookies-avela.png"
+          alt="Sabor Cookies e avela"
+          width={100}
+          height={100}
+          className={styles.image_card}
+        />
+        <h4>Sorvete Cookies & Avelã</h4>
+        <p>Delicioso sorvete sabor Cookies & Avelã. Uma explosão de sabor.</p>
+      </div>
+      <div className={styles.card_individual}>
+      <Image
+          src="/sorbet-Kiwi.png"
+          alt="Sabor Kiwi"
+          width={100}
+          height={100}
+          className={styles.image_card}
+        />
+        <h4>Sorvete de Kiwi</h4>
+        <p>Delicioso sorvete sabor Kiwi. Uma explosão de sabor.</p>
+      </div>
+      <div className={styles.card_individual}>
+      <Image
+          src="/sorbet-limao.png"
+          alt="Sabor Limão"
+          width={100}
+          height={100}
+          className={styles.image_card}
+        />
+        <h4>Sorvete de Limão Siciliano</h4>
+        <p>Delicioso sorvete sabor Limão Siciliano. Uma explosão de sabor.</p>
+      </div>
       </div>
     </section>
   );
