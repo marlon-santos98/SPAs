29ce8932b6aa87c3e45057e6f686635fb53bdde3
Marlon Santos <marlonadrianoteixeira@gmail.com>
2025-04-15 22:21:45 -0300

    add routes navbar

--- miniblog/src/components/Navbar.js
+++ miniblog/src/components/Navbar.js
@@ -1,13 +1,24 @@
 import React from 'react'
 import {NavLink} from 'react-router-dom'
 import styles from '../components/Navbar.module.css'
+import { useAuthentication } from '../hooks/useAuthentication'
+import { useAuthValue } from '../context/AuthContext'
 
 const Navbar = () => {
-  return <nav className={`${styles.navbar}`}>
+    const {user} = useAuthValue()
+
+    return <nav className={`${styles.navbar}`}>
     <NavLink to="/" className={`${styles.brand}`}>
         Mini <span>Blog</span>
     </NavLink>
     <ul className={`${styles.links_list}`}>
+        <li>
+            <NavLink to="/"  className={({isActive }) => (isActive ? styles.active : "")}>
+                Home
+            </NavLink>
+        </li>
+        {!user && (
+            <>
         <li>
             <NavLink to="/login"  className={({isActive }) => (isActive ? styles.active : "")}>
                 Entrar
@@ -18,11 +29,22 @@ const Navbar = () => {
                 Cadastrar
             </NavLink>
         </li>
-        <li>
-            <NavLink to="/"  className={({isActive }) => (isActive ? styles.active : "")}>
-                Home
-            </NavLink>
-        </li>
+            </>
+        )}
+        {user && (
+            <>
+                <li>
+                    <NavLink to="/posts/create"  className={({isActive }) => (isActive ? styles.active : "")}>
+                        Novo post
+                    </NavLink>
+                </li>
+                <li>
+                    <NavLink to="/dashboard"  className={({isActive }) => (isActive ? styles.active : "")}>
+                        Dashboard
+                    </NavLink>
+                </li>
+            </>
+        )}
         <li>
             <NavLink to="/about" className={({isActive }) => (isActive ? styles.active : "")}>
                 Sobre
