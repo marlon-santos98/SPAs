90471c4b35c28676a7d313e5674d4d3ac45796cf
Marlon Santos <marlonadrianoteixeira@gmail.com>
2025-04-28 21:29:13 -0300

    create post tag

--- miniblog/src/pages/Home/Home.js
+++ miniblog/src/pages/Home/Home.js
@@ -1,12 +1,39 @@
 import styles from './Home.module.css'
 import React from 'react'
 
+//hooks
+import {useNavigate, Link} from "react-router-dom"
+import { useState } from 'react'
+import { useFetchDocuments } from '../../hooks/useFetchDocuments'
+
+//components
+
 const Home = () => {
+  const [query, setQuery] = useState("")
+  const {documents: posts, loading} = useFetchDocuments("posts")
+  const handleSubmit = (e) => {
+    e.preventDefault()
+  }
   return (
-    <div>
-        <h1>Home</h1>
+    <div className={styles.home}>
+        <h1>Veja nossos posts mais recentes</h1>
+        <form onSubmit={handleSubmit} className={styles.search_form}>
+          <input type="text" placeholder='Ou busque por tags...' onChange={(e) => setQuery(e.target.value)}/>
+          <button className='btn btn-dark'>Pesquisar</button>
+        </form>
+          <div>
+            {loading && <p>Carregando..</p>}
+            {posts && posts.map((post) => (
+              <h3>{post.title}</h3>
+            ))}
+            {posts && posts.length == 0 &&(
+              <div className={styles.no_posts}>
+                <p>Não foram encontrados posts</p>
+                <Link to="/posts/create" className='btn'>Criar primeiro post</Link>
+              </div>
+            )}
+        </div>
     </div>
-  )
-}
+  )}
 
 export default Home
