5a67f98ef8034d629ea932e9d47467d39dd792d7
Marlon Santos <marlonadrianoteixeira@gmail.com>
2025-05-08 22:39:01 -0300

    added menu

--- miniblog/src/pages/Dashboard/Dashboard.js
+++ miniblog/src/pages/Dashboard/Dashboard.js
@@ -1,13 +1,49 @@
 import React from 'react'
 import styles from './Dashboard.module.css'
+import {Link} from 'react-router-dom'
+
+//hooks
+import {useAuthValue} from '../../context/AuthContext'
+import {useFetchDocuments} from '../../hooks/useFetchDocuments'
+import { HiH3 } from 'react-icons/hi2'
+
 
 const Dashboard = () => {
+  const {user} = useAuthValue()
+  const uid = user.uid
+  const {documents: posts, loading} = useFetchDocuments("posts", null, uid)
+  const deleteDocument = (id) => {
+      if(loading){
+        return <p>Carregando...</p>
+      }
+  }   
   return (
-    <>
-        <div>
+        <div className={styles.dashboard}>
             <h1>Dashboard</h1>
+            <p>Gerencie os seus posts</p>
+            {posts && posts.length == 0 ? (
+              <div className={styles.noposts}>
+                <p>Não foram encontrados posts</p>
+                <Link to="/posts/create" className='btn'>Criar primeiro post</Link>
+              </div>
+            ) : (
+              <>
+              <div className={styles.post_header}>
+                <span>Título</span>
+                <span>Ações</span>
+              </div>
+              {posts?.map((post)=><div key={post.id} className={styles.post_row}>
+                  <p>{post.title}</p>
+                  <div>
+                    <Link to={`/posts/${post.id}`} className='btn btn-outline'>Ver</Link>
+                    <Link to={`/posts/edit/${post.id}`} className='btn btn-outline'>Editar</Link>
+                    <button onClick={()=>deleteDocument(post.id)} className='btn btn-outline btn-danger'>Excluir</button>
+                  </div>
+              </div>)}
+              </>
+            )}
+
         </div>
-    </>
   )
 }
 
